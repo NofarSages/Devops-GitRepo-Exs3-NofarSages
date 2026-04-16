f5fe0e8c72c7bacf9cec21ccf3436c34538a36f0
NofarSages <nofarsages1@gmail.com>
2026-04-16 13:59:20 +0300

    Update nofar.java

--- nofar.java
+++ nofar.java
@@ -1,7 +1,7 @@
 public class nofar {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Nofar sages - Number: " + i);
+            System.out.println("Nofar sages - updated print: " + i);
         }
     }
 }
